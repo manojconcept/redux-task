0a10972731eb20ddd3618eac61724b16c8494aff
manojconcept <manojveerapavu@gmail.com>
2024-03-15 18:59:39 +0530

    fix

--- src/components/Layout/Cards.js
+++ src/components/Layout/Cards.js
@@ -1,21 +1,25 @@
 import Rating from '@mui/material/Rating';
 
 const Cards = ({ product,clickHandle }) => {
+    const {id,title,name,count,price,discountPercentage,stock} = product
+    const percentageAmout = (100 * discountPercentage) / price;
+    const productPriceA = price - percentageAmout;
+    const stockLeft = stock - count 
+    const countStock = stockLeft || stock
+    const quantity = count || 0
+    
 
-    const percentageAmout = (100 * product.discountPercentage) / product.price;
-    const productPriceA = product.price - percentageAmout;
-    const stockLeft = product.stock - product.count 
-    const count = stockLeft || product.stock
     return (
         <div className="col mb-5">
             <div className="card h-100">
                 <button type='button' style={{ border: "none", background: "none" }}>
-                    <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Stock : {count}</div>
+                    <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Stock : {countStock}</div>
+                    <div className="badge bg-secondary text-white position-absolute" style={{ top: "2rem", right: "0.5rem" }}>quantity  : {quantity}</div>
                     <img className="card-img-top" style={{ width: "100%", height: "200px" }} src={product.thumbnail} alt="..." />
                     <div className="card-body p-4">
                         <div className="text-center">
-                            <h5 className="fw-bolder">{product.name}</h5>
-                            <h5 className="fw-bolder">{product.title}</h5>
+                            <h5 className="fw-bolder">{name}</h5>
+                            <h5 className="fw-bolder">{title}</h5>
                             <Rating
                                 name="read-only"
                                 value={product.rating}
@@ -24,12 +28,12 @@ const Cards = ({ product,clickHandle }) => {
                             <div>
                                 <p>{product.description}</p>
                             </div>
-                            <span className="text-muted">₹<s>{product.price}</s>{` Offer : ₹${productPriceA.toFixed(2)}`}</span>
+                            <span className="text-muted">₹<s>{price}</s>{` Offer : ₹${productPriceA.toFixed(2)}`}</span>
                         </div>
                     </div>
                 </button>
                 <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div onClick={()=>clickHandle(product.id)} className="text-center"><button className="btn btn-outline-dark mt-auto" >
+                    <div onClick={()=>clickHandle(id)} className="text-center"><button className="btn btn-outline-dark mt-auto" >
                         Add to cart
                     </button></div>
                 </div>
