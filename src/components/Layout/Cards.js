18f6abf9c797cb0e74cab8a7defb5f35e091748c
manojconcept <manojveerapavu@gmail.com>
2024-03-15 19:02:31 +0530

    fix

--- src/components/Layout/Cards.js
+++ src/components/Layout/Cards.js
@@ -1,20 +1,20 @@
 import Rating from '@mui/material/Rating';
 
 const Cards = ({ product,clickHandle }) => {
+
     const {id,title,name,count,price,discountPercentage,stock} = product
     const percentageAmout = (100 * discountPercentage) / price;
     const productPriceA = price - percentageAmout;
     const stockLeft = stock - count 
     const countStock = stockLeft || stock
     const quantity = count || 0
-    
 
     return (
         <div className="col mb-5">
             <div className="card h-100">
                 <button type='button' style={{ border: "none", background: "none" }}>
                     <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Stock : {countStock}</div>
-                    <div className="badge bg-secondary text-white position-absolute" style={{ top: "2rem", right: "0.5rem" }}>quantity  : {quantity}</div>
+                    <div className="badge bg-secondary text-white position-absolute" style={{ top: "2rem", right: "0.5rem" }}>Added  : {quantity}</div>
                     <img className="card-img-top" style={{ width: "100%", height: "200px" }} src={product.thumbnail} alt="..." />
                     <div className="card-body p-4">
                         <div className="text-center">
